f2160d0c5cc9ce06f3481310835ae658236c29d0
sunandasharma61 <sunandasharma60101@gmail.com>
2023-10-25 15:58:40 +0530

    completed

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 # #----------- Route Table - Virtual Network Association
 resource "azurerm_subnet_route_table_association" "subnet_route_table_association" {
-    for_each             = local.subnet_list
-        subnet_id        = lookup(var.subnet_output,each.key,null) == null ? null : lookup(var.subnet_output,each.key,null).id
-        route_table_id   = lookup(var.route_table_output,each.value.route_table.name,null) == null ? null : ( lookup(var.route_table_output,each.value.route_table.name,null).resource_group_name == each.value.route_table.resource_group_name ? lookup(var.route_table_output,each.value.route_table.name,null).id :null)
+  for_each       = local.subnet_list
+  subnet_id      = lookup(var.subnet_output, each.key, null) == null ? null : lookup(var.subnet_output, each.key, null).id
+  route_table_id = lookup(var.route_table_output, each.value.route_table.name, null) == null ? null : (lookup(var.route_table_output, each.value.route_table.name, null).resource_group_name == each.value.route_table.resource_group_name ? lookup(var.route_table_output, each.value.route_table.name, null).id : null)
 }
--- output.tf
+++ output.tf
@@ -1,4 +1,4 @@
 output "snet_route_table_assoication_output" {
-    value = azurerm_subnet_route_table_association.subnet_route_table_association
-    description = "Outputs of subnet and  route table association object"
+  value       = azurerm_subnet_route_table_association.subnet_route_table_association
+  description = "Outputs of subnet and  route table association object"
 }
