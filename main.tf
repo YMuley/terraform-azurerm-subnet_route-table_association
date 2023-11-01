89e1ae0a0719160413ac80f09f62095bdeb95688
Yogesh Muley <Yogesh.Muley@MOTIFWORKS.IN>
2023-11-01 20:44:04 +0530

    code update

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 # #----------- Route Table - Virtual Network Association
 resource "azurerm_subnet_route_table_association" "subnet_route_table_association" {
-  for_each       = local.subnet_list
-  subnet_id      = lookup(var.subnet_output, each.key, null) == null ? null : lookup(var.subnet_output, each.key, null).id
-  route_table_id = lookup(var.route_table_output, each.value.route_table.name, null) == null ? null : (lookup(var.route_table_output, each.value.route_table.name, null).resource_group_name == each.value.route_table.resource_group_name ? lookup(var.route_table_output, each.value.route_table.name, null).id : null)
+  for_each       = local.route_table
+  route_table_id = var.route_table_output[each.value.route_table_name].id
+  subnet_id = var.subnet_output[each.value.subnet_id].id
 }
