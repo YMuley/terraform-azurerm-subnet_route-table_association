416d136ce5addfa1964cf9b18067007b3c96b654
Yogesh Muley <Yogesh.Muley@MOTIFWORKS.IN>
2024-05-27 17:29:41 +0530

    code update

--- locals.tf
+++ locals.tf
@@ -1,4 +1,4 @@
 locals {
-  ##route_table = { for route_table in var.subnet_route_table_association_list : format("%s-%s-%s", route_table.route_table_name, route_table.subnet_name, route_table.virtual_network_name) => route_table }
-  route_table = { for route_table in var.subnet_route_table_association_list : route_table.route_table_name => route_table}
+  route_table = { for route_table in var.subnet_route_table_association_list : format("%s-%s-%s", route_table.route_table_name, route_table.subnet_name, route_table.virtual_network_name) => route_table }
+  ##route_table = { for route_table in var.subnet_route_table_association_list : route_table.route_table_name => route_table}
 }
